bf1e78180f203adaa01e82ad098fe6e5c32e6b82
Aidan Jack <ajack@sms-plc.com>
2018-03-01 17:02:02 +0000

    everted changes to assemblyinfo

--- Rebus.AmazonSQS/Properties/AssemblyInfo.cs
+++ Rebus.AmazonSQS/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rebus.AmazonSQS")]
@@ -15,19 +14,19 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("586dc77b-804e-4b66-8a2f-33ce49362a27")]
+[assembly: Guid("586dc77b-804e-4b66-8a2f-33ce49362a27")] 
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
